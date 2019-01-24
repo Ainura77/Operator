648393844b56c4fe0e769409c1561749cc03bb5d
Ainura77 <araoufi77@gmail.com>
2019-01-24 17:34:00 -0500

    Subtract method added to the code

--- src/Operator.java
+++ src/Operator.java
@@ -2,11 +2,18 @@ public class Operator {
 
     public static void main(String[] args) {
 
-        int temp = Add(100,300);
+        int temp = Add(100, 300);
         System.out.println(temp);
     }
-        public static int Add(int x, int y){
-            int sum = x + y;
-            return sum;
+
+    public static int Add(int x, int y) {
+        int sum = x + y;
+        return sum;
+    }
+
+    public static int Subtract(int x, int y) {
+        int sum = x + y;
+        return sum;
+
     }
 }
